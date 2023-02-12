34b419872c5c58047e3dbfa3b39a5c62dacc7220
13837534822 <527958745@qq.com>
2023-02-13 00:17:30 +0800

    帅哥帅哥

--- Tree.c
+++ Tree.c
@@ -3,7 +3,7 @@
 # define MaxSize 100 
 //顺序存储结构
 //------------------------------------------------------------------
-//顺序结构的节点
+//顺序结构的节点 （从1位置开始存）
 typedef struct Arr_Node{
     int value;
     bool isEmpty;
@@ -31,9 +31,34 @@ int GetRight(int i){
         return 2*i+1;
     }
 }
+int GetParent(int i){
+    if(i/2<1){
+        return -1;
+    }else{
+        return i/2;
+    }
+}
+
+//----------------------------------------------------------------
+//链式存储
+typedef struct Link_Node{
+    int data;
+    struct Link_Node *left;
+    struct Link_Node *right;
+}Link_Node;
+
+
 int main(){
     Arr_Node t[MaxSize];
-    Init_ArrTree(&t);
-    printf("main:%d",GetLeft(60));
+    // Init_ArrTree(&t);
+    // printf("main:%d",GetParent(1));
+    Link_Node node1={1};
+    Link_Node node2={2};
+    Link_Node node3={3};
+    Link_Node node4={4};
+    node1.left=&node2;
+    node1.right=&node3;
+    node2.left=&node4;
+    printf("%d",node1.left->left->data);
     return 0;
 }
